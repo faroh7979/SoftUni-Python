195e609d19b50e410145153261ff40f7e845653b
faroh7979 <faroh797979@gmail.com>
2022-08-25 19:29:38 +0300

    Update 07.food_delivery.py

--- programing_basics/01.first_steps_in_coding_exercise/07.food_delivery.py
+++ programing_basics/01.first_steps_in_coding_exercise/07.food_delivery.py
@@ -4,11 +4,11 @@ vegie_menus = int(input())
 
 chicken_price = 10.35
 fish_price = 12.40
-vegie_price = 8.15
+veggie_price = 8.15
 
 menus_sum = (chicken_menus * chicken_price) + \
             (fish_price * fish_menus) + \
-            (vegie_price * vegie_menus)
+            (veggie_price * vegie_menus)
 
 desert = menus_sum * 0.2
 delivery = 2.50
