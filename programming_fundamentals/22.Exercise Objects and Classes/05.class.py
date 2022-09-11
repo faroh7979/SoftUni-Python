fcbde2c0733698a199dfe15260f63a2885dab7f9
faroh7979 <faroh797979@gmail.com>
2022-09-11 15:33:21 +0300

    Create 05.class.py

---
+++ programming_fundamentals/22.Exercise Objects and Classes/05.class.py
@@ -0,0 +1,21 @@
+class Class:
+    __students_count = 22
+
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        self.grades = []
+
+    def add_student(self, name, grade):
+        if Class.__students_count > 0:
+            self.students.append(name)
+            self.grades.append(grade)
+            Class.__students_count -= 1
+
+    def get_average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
+    def __repr__(self):
+        return f'The students in {self.name}: {", ".join(self.students)}. Average grade: {self.get_average_grade():.2f}'
+
+
