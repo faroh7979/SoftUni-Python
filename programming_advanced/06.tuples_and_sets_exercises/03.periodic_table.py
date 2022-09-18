822b7f2899f22eac5f897821b352a63a47cf22f5
faroh7979 <faroh797979@gmail.com>
2022-09-18 10:51:46 +0300

    Update 03.periodic_table.py

--- programming_advanced/06.tuples_and_sets_exercises/03.periodic_table.py
+++ programming_advanced/06.tuples_and_sets_exercises/03.periodic_table.py
@@ -1,11 +1,10 @@
-total_names = int(input())
-all_names = []
+total_inputs = int(input())
+unique_elements = set()
 
-for _ in range(total_names):
-    name = input()
-    all_names.append(name)
+for _ in range(total_inputs):
+    current_elements = input().split()
+    for element in current_elements:
+        unique_elements.add(element)
 
-unique_names = set(all_names)
-
-for unique_name in unique_names:
-    print(unique_name)
+for chemical_element in unique_elements:
+    print(chemical_element)
