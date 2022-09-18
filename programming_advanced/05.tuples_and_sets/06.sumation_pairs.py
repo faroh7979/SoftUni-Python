eb3543d34b45abd9ef9f46842004a46c45adaf1f
faroh7979 <faroh797979@gmail.com>
2022-09-18 18:49:16 +0300

    Update 06.sumation_pairs.py

--- programming_advanced/05.tuples_and_sets/06.sumation_pairs.py
+++ programming_advanced/05.tuples_and_sets/06.sumation_pairs.py
@@ -1,24 +1,24 @@
 sequence_of_numbers = list(map(int, input().split()))
 target_number = int(input())
-second_list = sequence_of_numbers[1:]
-final_list = []
-winning_pairs = False
+used_in_winning_pairs = []
+winning_pairs = []
 
 for current_index in range(len(sequence_of_numbers)):
+    if current_index in used_in_winning_pairs:  # in case that this value has been already used
+        continue
+
     first_num = sequence_of_numbers[current_index]
 
-    for second_index in range(len(second_list)):
-        second_num = second_list[second_index]
+    for index_second_num in range(current_index + 1, len(sequence_of_numbers)):
+        if index_second_num in used_in_winning_pairs:  # again in case that this value has been already used
+            continue
 
-        if first_num + second_num == target_number:
-            final_list.append(f'{first_num} + {second_num} = {target_number}')
-            sequence_of_numbers.remove(second_num)
-            second_list.remove(first_num)
-            second_list.remove(second_num)
+        second_num = sequence_of_numbers[index_second_num]
+
+        if first_num + second_num == target_number:  # winning pair
+            winning_pairs.append(f'{first_num} + {second_num} = {target_number}')
+            used_in_winning_pairs.append(current_index)
+            used_in_winning_pairs.append(index_second_num)
             break
 
-    sequence_of_numbers.remove(first_num)
-    if not sequence_of_numbers:
-        break
-
-print("\n".join(final_list))
+print("\n".join(winning_pairs))
