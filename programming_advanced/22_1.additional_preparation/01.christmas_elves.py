84906f86d14b6785ba152d668ba5776a76ddab6a
faroh7979 <faroh797979@gmail.com>
2022-10-18 18:07:38 +0300

    Create 01.christmas_elves.py

---
+++ programming_advanced/22_1.additional_preparation/01.christmas_elves.py
@@ -0,0 +1,68 @@
+from collections import deque
+
+elves = deque([int(i) for i in input().split()])
+energy = [int(i) for i in input().split()]
+total_toys = 0
+total_used_energy = 0
+
+current_craft_times = 0
+while elves and energy:
+    current_craft_times += 1
+    current_elf = elves.popleft()
+    current_energy = energy.pop()
+
+    if current_elf < 5:  # Careful if this case is counting for the crafted
+        continue
+
+    elif current_craft_times % 15 == 0:
+        current_energy *= 2
+        if current_energy <= current_elf:
+            current_elf -= current_energy
+            total_used_energy += current_energy
+            current_elf += 1
+
+        else:
+            current_elf *= 2
+        elves.append(current_elf)
+        energy.append(current_energy)
+
+    elif current_craft_times % 3 == 0:
+        current_energy *= 2
+        if current_energy <= current_elf:
+            total_toys += 2
+            current_elf -= current_energy
+            total_used_energy += current_energy
+            current_elf += 1
+
+        else:
+            current_elf *= 2
+        elves.append(current_elf)
+        energy.append(current_energy)
+
+    elif current_craft_times % 5 == 0:
+        if current_energy <= current_elf:
+            current_elf -= current_energy
+            total_used_energy += current_energy
+        else:
+            current_elf *= 2
+        elves.append(current_elf)
+        energy.append(current_energy)
+
+    else:
+        if current_energy <= current_elf:
+            current_elf -= current_energy
+            total_used_energy += current_energy
+            total_toys += 1
+            current_elf += 1
+        else:
+            current_elf *= 2
+        elves.append(current_elf)
+        energy.append(current_energy)
+
+print(f'Toys: {total_toys}')
+print(f'Energy: {total_used_energy}')
+
+if elves:
+    print(f'Elves left: {", ".join(map(str, elves))}')
+elif energy:
+    print(f'Boxes left: {", ".join(map(str, energy))}')
