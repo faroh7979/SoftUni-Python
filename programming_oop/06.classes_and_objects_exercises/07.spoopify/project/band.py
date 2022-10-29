fa40daba1870be9e0041e3dd9291568ee8c7271b
faroh7979 <faroh797979@gmail.com>
2022-10-29 20:14:10 +0300

    oop

---
+++ programming_oop/06.classes_and_objects_exercises/07.spoopify/project/band.py
@@ -0,0 +1,48 @@
+from album import Album
+from song import Song
+
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+
+        self.albums.append(album)
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str):
+
+        try:
+            searched_album = next(filter(lambda a: a.name == album_name, self.albums))
+
+        except StopIteration:
+            return f"Album {album_name} is not found."
+
+        if searched_album.published:
+            return "Album has been published. It cannot be removed."
+
+        self.albums.remove(searched_album)
+        return f"Album {album_name} has been removed."
+
+    def details(self):
+        list_info = [f'Band {self.name}']
+        list_info.extend([a.details() for a in self.albums])
+        return '\n'.join(list_info)
+
+
+song = Song("Running in the 90s", 3.45, False)
+print(song.get_info())
+album = Album("Initial D", song)
+second_song = Song("Around the World", 2.34, False)
+print(album.add_song(second_song))
+print(album.details())
+print(album.publish())
+band = Band("Manuel")
+print(band.add_album(album))
+print(band.remove_album("Initial D"))
+print(band.details())
