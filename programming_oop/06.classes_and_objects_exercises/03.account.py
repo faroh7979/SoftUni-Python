f5819c571cf1c012e86d0100e5f2e06c219bfac4
faroh7979 <faroh797979@gmail.com>
2022-10-29 09:26:11 +0300

    Create 03.account.py

---
+++ programming_oop/06.classes_and_objects_exercises/03.account.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, id_number: int, name: str, balance=0):
+        self.id = id_number
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def debit(self, debit_amount):
+
+        if self.balance >= debit_amount:
+            self.balance -= debit_amount
+            return self.balance
+
+        return "Amount exceeded balance"
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+
+
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
