3ac3d5bb6912b9a5971b09963e69aedbe1d18ec9
faroh7979 <faroh797979@gmail.com>
2022-10-30 07:36:21 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,26 @@
-cska = [1, 2, 3, 4, 5, 6]
+output = '''Go to University
+28.05.2020
+Don't forget laptop and notebook
+Name: Go to University - Due Date: 28.05.2020
+Task Name: Go to University - Due Date: 28.05.2020 is added to the section
+Cleared 0 tasks.
+Section Daily tasks:
+Name: Go to University - Due Date: 28.05.2020
+Name: Make bed - Due Date: 27/05/2020'''
 
-cska[] = 'e'
 
-print(cska)
+expcted_output = '''Go to University
+28.05.2020
+Don't forget laptop and notebook
+Name: Go to University - Due Date: 28.05.2020
+Task Name: Go to University - Due Date: 28.05.2020 is added to the section
+Cleared 0 tasks.
+Section Daily tasks:
+Name: Go to University - Due Date: 28.05.2020
+Name: Make bed - Due Date: 27/05/2020'''
+
+# for current_index in range(len(expcted_output)):
+#     if output[current_index] != expcted_output[current_index]:
+#         print(current_index)
+
+print(expcted_output == output)
