5fc4c29424cf2dad0d26c9400f0524c484c27c93
faroh7979 <faroh797979@gmail.com>
2022-11-08 20:05:25 +0200

    Update food.py

--- programming_oop/10.encapsulation_exercises/04.restaurant/project/food/food.py
+++ programming_oop/10.encapsulation_exercises/04.restaurant/project/food/food.py
@@ -1,1 +1,11 @@
-from 
+from project.product import Product
+
+
+class Food(Product):
+    def __init__(self, name: str, price: float, grams: float):
+        super().__init__(name, price)
+        self.__grams = grams
+
+    @property
+    def grams(self):
+        return self.__grams
