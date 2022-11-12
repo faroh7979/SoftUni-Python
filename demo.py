ef6107bc6d1c1ea49a364bc4e619b90d6b9abf46
faroh7979 <faroh797979@gmail.com>
2022-11-12 10:42:47 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,3 @@
-solution = False
-for x in range(-100, 100):
+a ='a'
 
-    if solution:
-        break
-
-    for y in range(-100, 100):
-        if 2 * x - 3 * y == 8:
-            if 3 * x - 4 * y == 14:
-                solution = True
-                print(f'Triangle = {x}\nSquare = {y}')
-                break
+print(int(a))
