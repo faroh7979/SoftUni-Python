d05817a157480a4895b300eb304e92e9c2c8f0aa
faroh7979 <faroh797979@gmail.com>
2022-11-15 20:41:28 +0200

    Create 03.playing.py

---
+++ programming_oop/13.polymorphism_and_abstraction/03.playing.py
@@ -0,0 +1,13 @@
+def start_playing(obj):
+    return obj.play()
+
+
+class Guitar:
+
+    @staticmethod
+    def play():
+        return "Playing the guitar"
+
+
+guitar = Guitar()
+print(start_playing(guitar))
