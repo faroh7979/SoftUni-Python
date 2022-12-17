38802f624df097aaf4c34e44269394454a9dbca8
faroh7979 <faroh797979@gmail.com>
2022-12-17 18:00:56 +0200

    Create private_booth.py

---
+++ programming_oop/28.regular_exam/01. Structure_Skeleton/project/booths/private_booth.py
@@ -0,0 +1,12 @@
+from project.booths.booth import Booth
+
+
+class PrivateBooth(Booth):
+    PRIZE_PER_PERSON_FOR_RESERVATION = 3.50
+
+    def __init__(self, booth_number: int, capacity: int):
+        super().__init__(booth_number, capacity)
+
+    def reserve(self, number_of_people: int):
+        self.price_for_reservation = number_of_people * self.PRIZE_PER_PERSON_FOR_RESERVATION
+        self.is_reserved = True
